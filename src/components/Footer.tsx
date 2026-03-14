563fd948997f3ee1020e8b96cc4f70a3749086e9
Tatiana <t.ya14091984@gmail.com>
2026-03-14 15:38:17 +0200

    Introduce routing with `react-router`, refactor components for dynamic hero-based navigation and error handling.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,19 @@
 import Button from "./ui/Button.tsx";
+import {NavLink} from "react-router";
+import {navItems} from "../utils/constants.ts";
+import {useContext} from "react";
+import {NavContext} from "../utils/context.ts";
 
 
 const Footer = () => {
+    const {name} = useContext(NavContext)
     return (
         <footer className="clear-both rounded-b-3xl bg-gray h-20 grid grid-cols-10 items-center text-center">
-            <Button
-                className="col-start-3">
-                Send me email
-            </Button>
+            <NavLink className="col-start-3" to={`${navItems[3].toLowerCase()}/${name}`}>
+                <Button>
+                    Send me email
+                </Button>
+            </NavLink>
         </footer>
     );
 };
